7e2115bf359ad41d92189510e82d20a66bf6b6f2
adornadowilliam@gmail.com <adornadowilliam@gmail.com>
2024-05-28 06:02:32 -0700

    finished version

--- app/index.jsx
+++ app/index.jsx
@@ -15,42 +15,42 @@ const Index = () => {
           <View style={Styles.body}>
             <View style={Styles.content}>
               <View style={Styles.content}>
-                <View style={styles.view1}>
-                  <Text style={Styles.colorred}>Text 1</Text>
+                <View style={styles.primary}>
+                  <Text style={Styles.text2}>1</Text>
                 </View>
 
-                <View style={styles.view2}>
-                  <Text style={Styles.text2}>Text 2</Text>
+                <View style={[styles.view2, styles.purple]}>
+                  <Text style={Styles.text2}>2</Text>
                 </View>
               </View>
 
-              <View style={styles.view3}>
+              <View style={[styles.view3, styles.yellow]}>
                 <Text
                   style={
                     Styles.text2 //global style
                   }
                 >
-                  Text 3
+                  3
                 </Text>
               </View>
             </View>
             {/* Another content */}
           </View>
-          <View style={styles.view1}>
-            <Text style={Styles.text2}>Text 1</Text>
+          <View style={[styles.view1, styles.red]}>
+            <Text style={Styles.text2}>4</Text>
           </View>
-          <View style={styles.view2}>
-            <Text style={Styles.text2}>Text 2</Text>
+          <View style={[styles.view2, styles.green]}>
+            <Text style={Styles.text2}>5</Text>
           </View>
         </View>
         {/* Content 3 */}
         <View style={Styles.body}>
-          <View style={styles.view1}>
-            <Text style={Styles.text2}>Text 1</Text>
+          <View style={[styles.view1,styles.lightblue]}>
+            <Text style={Styles.text2}>6</Text>
           </View>
 
-          <View style={styles.view2}>
-            <Text style={Styles.text2}>Text 2</Text>
+          <View style={[styles.view2,styles.darkblue]}>
+            <Text style={Styles.text2}>7</Text>
           </View>
         </View>
       </ScrollView>
@@ -59,25 +59,44 @@ const Index = () => {
 };
 
 const styles = StyleSheet.create({
-  text: { textTransform: "uppercase", color: "#000", fontWeight: "bold" },
+  primary:{
+    paddingLeft: "10%", 
+    paddingRight: "10%", 
+    backgroundColor: "#7beeff"
+  },
   view1: {
     flex: 1,
-    backgroundColor: "#0ff",
     alignItems: "center",
     justifyContent: "center",
   },
   view2: {
     flex: 1,
-    backgroundColor: "#f0f",
     alignItems: "center",
     justifyContent: "center",
   },
   view3: {
     flex: 1,
-    backgroundColor: "#ff0",
     alignItems: "center",
     justifyContent: "center",
   },
+  purple:{
+    backgroundColor: "#a538f5",
+  },
+  yellow:{
+    backgroundColor: "yellow",
+  },
+  red:{
+    backgroundColor:"red"
+  },
+  green:{
+    backgroundColor:"#71e834"
+  },
+  lightblue:{
+    backgroundColor:"#b8fff2"
+  },
+  darkblue:{
+    backgroundColor:"#392ceb"
+  }
 });
 
 export default Index;
